17fcea21d9baa5fac9ed66fe21ee679f0f32b0fd
ashrafiabir01 <ashrafiabir04@gmail.com>
2022-03-16 01:38:48 +0600

    hi

--- lib/core/homepage.dart
+++ lib/core/homepage.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:romjan_protidin/core/customWidget/appbar.dart';
 
@@ -25,7 +23,7 @@ class _HomePageState extends State<HomePage> {
     var responsive_height = MediaQuery.of(context).size.height;
     var responsive_width = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Color(0xFF00B563),
+      backgroundColor: Color(0xFF647AFF),
       appBar: CustomAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(1.0),
@@ -40,7 +38,7 @@ class _HomePageState extends State<HomePage> {
                       width: double.infinity,
                       height: double.infinity,
                       decoration: BoxDecoration(
-                          color: Color(0xFF00B563),
+                          color: Color(0xFF647AFF),
                           border: Border(
                               left: BorderSide(color: Colors.white, width: 2),
                               right:
@@ -71,7 +69,7 @@ class _HomePageState extends State<HomePage> {
                   width: double.infinity,
                   height: double.infinity,
                   decoration: BoxDecoration(
-                      color: Color(0xFF00B563),
+                      color: Color(0xFF647AFF),
                       border: Border(
                           left: BorderSide(color: Colors.white, width: 2),
                           right: BorderSide(color: Colors.white, width: 2))),
@@ -79,28 +77,48 @@ class _HomePageState extends State<HomePage> {
                     padding: const EdgeInsets.only(right: 6, left: 6),
                     child: Container(
                       decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.white.withOpacity(0.45),
                           borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10))),
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20))),
                       child: Row(
                         children: [
                           Expanded(
-                            flex: 33,
+                            flex: 50,
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 16),
-                                  child: Text("সন্ধা ৬:১৭ মিনিট",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 27)),
-                                ),
-                                Container(
-                                  color: Colors.red,
-                                  width: responsive_width / 1,
-                                  height: 38,
+                                Expanded(
+                                    flex: 5,
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(top: 16),
+                                      child: Flexible(
+                                        child: Text("রাত ৬:১৭ মিনিট",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize:
+                                                    responsive_width / 35)),
+                                      ),
+                                    )),
+                                Expanded(
+                                  flex: 3,
+                                  child: Container(
+                                    color: Colors.white.withOpacity(0.3),
+                                    width: responsive_width / 1,
+                                    height: 38,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Flexible(
+                                            child: Text("সাহরী",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Colors.black,
+                                                    fontSize: 17)),
+                                          )),
+                                    ),
+                                  ),
                                 )
                               ],
                             ),
@@ -109,16 +127,46 @@ class _HomePageState extends State<HomePage> {
                             flex: 1,
                             child: Container(
                                 width: responsive_width / 70,
-                                color: Color(0xFF00B563)),
+                                color: Color(0xFF647AFF)),
                           ),
                           Expanded(
-                            flex: 33,
+                            flex: 50,
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text("সাহরী"),
+                                Expanded(
+                                  flex: 5,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 16),
+                                    child: Flexible(
+                                      child: Text("সন্ধা ৬:১৭ মিনিট",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: responsive_width / 35)),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Container(
+                                    color: Colors.white.withOpacity(0.3),
+                                    width: responsive_width / 1,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Flexible(
+                                            child: Text("ইফতার",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.w400,
+                                                    fontSize: 17)),
+                                          )),
+                                    ),
+                                  ),
+                                )
                               ],
                             ),
-                          )
+                          ),
                         ],
                       ),
                     ),
@@ -129,7 +177,7 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                     child: Container(
                   decoration: BoxDecoration(
-                      color: Color(0xFF00B563),
+                      color: Color(0xFF647AFF),
                       border: Border(
                           left: BorderSide(color: Colors.white, width: 2),
                           right: BorderSide(color: Colors.white, width: 2))),
@@ -139,7 +187,7 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                     child: Container(
                   decoration: BoxDecoration(
-                      color: Color(0xFF00B563),
+                      color: Color(0xFF647AFF),
                       border: Border(
                           left: BorderSide(color: Colors.white, width: 2),
                           right: BorderSide(color: Colors.white, width: 2))),
@@ -149,7 +197,7 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                     child: Container(
                   decoration: BoxDecoration(
-                      color: Color(0xFF00B563),
+                      color: Color(0xFF647AFF),
                       border: Border(
                           left: BorderSide(color: Colors.white, width: 2),
                           bottom: BorderSide(color: Colors.white, width: 2),
